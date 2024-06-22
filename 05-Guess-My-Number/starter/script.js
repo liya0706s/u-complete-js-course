7858da3ebd372537f9470ca22e4612dc1b30d08a
Angie <liya0706s@gmail.com>
2024-06-22 21:28:37 +0800

    again btn haven't finished

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,47 +12,59 @@
 // document.querySelector('.guess').value=23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
-let score=20;
-document.querySelector('.number').textContent=secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+// define again btn function
+document.querySelector('.again').addEventListener
+    ('click', function () {
+        document.querySelector('.message').textContent='Start guessing...';
+        document.querySelector('.number').textContent='?';
+        document.querySelector('.guess').value='';
+        // document.querySelector('.score').textContent='20';
+        score=20;
+        document.querySelector('body').style.backgroundColor='black';
+    });
 
 document.querySelector('.check').addEventListener
-('click', function(){
-    const guess= Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
-    // 如果沒有放上Number(),通常會是string
+    ('click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(guess, typeof guess);
+        // 如果沒有放上Number(),通常會是string
 
-    // When there is no input
-    if(!guess){
-        document.querySelector('.message').textContent='No number! 🫥';
-        
-    // When player wins
-    }else if(guess === secretNumber){
-        document.querySelector('.message').textContent='Correct Number! 🙌';
-        document.querySelector('body').style.backgroundColor='#60b347';
-        document.querySelector('.number').style.width='30rem';
+        // When there is no input
+        if (!guess) {
+            document.querySelector('.message').textContent = 'No number! 🫥';
 
-    // When guess is too high
-    }else if(guess>secretNumber){
-        if(score>1){
-            document.querySelector('.message').textContent='🎈 Too high!';
-            score--;
-            document.querySelector('.score').textContent=score;
-        }else{
-            document.querySelector('.message').textContent='You lost the game!';
-            document.querySelector('.score').textContent=0;
+            // When player wins
+        } else if (guess === secretNumber) {
+            document.querySelector('.message').textContent = 'Correct Number! 🙌';
+            document.querySelector('.number').textContent = secretNumber;
+
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+
+            // When guess is too high
+        } else if (guess > secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = '🎈 Too high!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the game!';
+                document.querySelector('.score').textContent = 0;
+            }
+
+            // When guess is too low
+        } else if (guess < secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = '😶‍🌫️ Too low!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the game!';
+                document.querySelector('.score').textContent = 0;
+            }
         }
-    
-        // When guess is too low
-    }else if(guess<secretNumber){
-        if(score>1){
-            document.querySelector('.message').textContent='😶‍🌫️ Too low!';
-            score--;
-            document.querySelector('.score').textContent=score;
-        }else{
-            document.querySelector('.message').textContent='You lost the game!';
-            document.querySelector('.score').textContent=0;
-        }
-    }
-});
+    });
 
