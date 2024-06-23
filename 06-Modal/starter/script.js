e71191b69df9ae9223c21521ed1438b1c6620387
Angie <liya0706s@gmail.com>
2024-06-23 16:10:41 +0800

    working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
-const modal=document.querySelector('.modal');
-const overlay=document.querySelector('.overlay');
-const btnCloseModal=document.querySelector('.close-modal');
-const btnsOpenModal=document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal=function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
 // 如果只須執行一行程式碼，不用花括號
-for(let i=0; i<btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+
+
