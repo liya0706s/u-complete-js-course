a5d4da8255476394482e54d56870a479b1562eed
Angie <liya0706s@gmail.com>
2024-06-23 17:38:04 +0800

    handling an esc keydown event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,13 +5,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal=function () {
+const openModal = function () {
     console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
 
-const closeModal=function(){
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
@@ -20,8 +20,17 @@ const closeModal=function(){
 for (let i = 0; i < btnsOpenModal.length; i++)
     btnsOpenModal[i].addEventListener('click', openModal);
 
-    btnCloseModal.addEventListener('click', closeModal);
-    overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+    // console.log('A key was pressed');
+    console.log(e.key);
 
-
+    if (e.key === 'Escape') {
+        // console.log('Esc was pressed');
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+});
